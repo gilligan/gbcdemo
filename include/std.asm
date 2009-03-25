a4e4875a33e926b6cc354a0b0ca9558434d17efa
Tobias Pflug <gilligan-@gmx.net>
2009-03-25 23:34:18 +0100

    * added dma_copy routine to std.asm

--- include/std.asm
+++ include/std.asm
@@ -84,3 +84,104 @@ read_joypad:
 	ldh [$00],a  ; <- RESET Joypad
 	ret           ; <- Return from Subroutine
 
+
+;
+; stupid macro to do (bc>>n)
+;
+.macro shr16_bc
+        .rept \1
+                srl b
+                rr c
+        .endr
+.endm
+
+;
+; stupid macro to do (bc<<n)
+;
+.macro shl16_bc
+        .rept \1
+        sla c
+        rl b
+        .endr
+.endm
+
+
+;
+; copy data via DMA
+; 
+; in       : hl = src, de = dst, bc = number of bytes (mod16==0) / max == 4096
+; out      : --
+; destroys : uhm.. everything :)
+;
+dma_copy:
+        shr16_bc 4 ; bc = count/16
+
+        ld a,c
+        sub 128
+        jp nc,chain_dma
+
+        ld a,h
+        ld [HDMA1],a
+        ld a,l
+        ld [HDMA2],a
+        ld a,d
+        ld [HDMA3],a
+        ld a,e
+        ld [HDMA4],a
+        ld a,c
+        dec a 
+        and $7f
+        ld [HDMA5],a
+        ret
+
+chain_dma:
+
+        ld a,c
+        and $80
+        jp nz,above_max
+        ld a,c
+above_max:
+        ld b,a         ; b = cnt = (count>128)?128:count;
+
+        ld a,h
+        ld [HDMA1],a
+        ld a,l
+        ld [HDMA2],a
+        ld a,d
+        ld [HDMA3],a
+        ld a,e
+        ld [HDMA4],a
+        ld a,b
+        dec a 
+        and $7f
+        ld [HDMA5],a
+
+        ld a,c
+        sub b            ; count -= cnt;
+        ret z            ; if (count == 0 ) return;
+        ld c,a
+
+        push bc
+
+        ld a,b
+        ld c,a
+        ld b,0
+        shl16_bc 4       ; bc = cnt*16;
+
+        ld a,l
+        add c
+        ld l,a
+        ld a,h
+        adc b
+        ld h,a           ; hl += cnt*16
+
+        ld a,e
+        add c
+        ld e,a
+        ld a,d
+        adc b
+        ld d,a           ; de += cnt*16
+
+        pop bc
+        jp chain_dma
+
